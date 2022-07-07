2436dc32ff3fb7435f63bbac53b616211f4bfed2
ByteCntrl <bytecntrl@gmail.com>
2022-07-07 13:03:29 +0200

    Initial commit

---
+++ src/routes/login/index.svelte
@@ -0,0 +1,29 @@
+<svelte:head>
+	<title>Login</title>
+</svelte:head>
+
+<div class="container align-items-center mt-3">
+	<form>
+		<div class="form-group mb-3">
+			<label for="UsernameInput">Username</label>
+			<input
+				type="text"
+				class="form-control"
+				id="UsernameInput"
+				placeholder="Input your username"
+				name="Username"
+			/>
+		</div>
+		<div class="form-group mb-3">
+			<label for="PasswordInput">Password</label>
+			<input
+				type="password"
+				class="form-control"
+				id="PasswordInput"
+				placeholder="Input your password"
+				name="Password"
+			/>
+		</div>
+		<button class="btn btn-primary">Login</button>
+	</form>
+</div>
